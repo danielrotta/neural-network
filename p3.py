cb64d7587394f00ecf6eae6a44190863e60f6611
danielrotta <danielrotta13@gmail.com>
2020-06-22 19:35:48 -0500

    lesson 5

--- p3.py
+++ p3.py
@@ -29,6 +29,7 @@ X = [[1, 2, 3, 2.5],\
     [2.0, 5.0, -1.0, 2.0],\
     [-1.5, 2.7, 3.3, -0.8]]
 
+'''
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
@@ -44,3 +45,12 @@ print(layer1.output)
 
 layer2.forward(layer1.output)
 print(layer2.output)
+'''
+
+inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
+output = []
+
+for i in inputs:
+    output.append(max(i,0))
+
+print(output)
